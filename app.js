096091e05d972fafdc392ae7f4f9ad005ceb7745
Aman2104 <amanyara88@gmail.com>
2024-02-07 14:28:53 +0530

    Test again

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const connectToMongo = require('./db');
 const admin = require('firebase-admin')
 const cors = require('cors')
 const userRoutes = require('./routes/user');
 const connectionRoute = require('./routes/connect');
 const Connection = require('./models/Connect');
+require('dotenv').config()
+const connectToMongo = require('./db');
 
 
 connectToMongo()
 const app = express();
 const User = require('./models/User');
-require('dotenv').config()
 
 
 
@@ -66,59 +66,59 @@ app.use('/api/connection', connectionRoute);
 
 
 
-const saveMessageToDatabase = async (data) => {
-  try {
-    const { senderId, content, userA, userB } = data;
+// const saveMessageToDatabase = async (data) => {
+//   try {
+//     const { senderId, content, userA, userB } = data;
 
-    const connectionAtoB = await Connection.findOne({ currentUser: userA, requestUser: userB });
-    const connectionBtoA = await Connection.findOne({ currentUser: userB, requestUser: userA });
+//     const connectionAtoB = await Connection.findOne({ currentUser: userA, requestUser: userB });
+//     const connectionBtoA = await Connection.findOne({ currentUser: userB, requestUser: userA });
 
-    connectionAtoB.messages.push({
-      senderId,
-      content,
-    });
-    connectionBtoA.messages.push({
-      senderId,
-      content,
-    });
+//     connectionAtoB.messages.push({
+//       senderId,
+//       content,
+//     });
+//     connectionBtoA.messages.push({
+//       senderId,
+//       content,
+//     });
 
-    await connectionAtoB.save();
-    await connectionBtoA.save();
-  } catch (error) {
-    console.error('Error saving message to database:', error);
-  }
-};
-
-
+//     await connectionAtoB.save();
+//     await connectionBtoA.save();
+//   } catch (error) {
+//     console.error('Error saving message to database:', error);
+//   }
+// };
 
 
-app.post('/sendNotification', async (req, res) => {
-  try {
-    const { id, code } = req.body
-    const user = await User.findById(id);
-    if (!user) {
-      console.error(req.body);
-      return res.status(404).send('User not found');
-    }
-    const payload = {
-      tokens: [user.fcmToken],
-      notification: {
-        title: 'Basic Notification',
-        body: 'This is a basic notification sent from the server!',
-      },
-      data: {
-        receiverId: id,
-        code: code,
-        navigationTarget: 'VideoCall',
-      }
-    };
-    const response = await admin.messaging().sendMulticast(payload);
 
-    console.log('Successfully sent multicast notification:', response);
-    res.status(200).send('Multicast notification sent successfully');
-  } catch (error) {
-    console.error('Error sending multicast notification:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+
+// app.post('/sendNotification', async (req, res) => {
+//   try {
+//     const { id, code } = req.body
+//     const user = await User.findById(id);
+//     if (!user) {
+//       console.error(req.body);
+//       return res.status(404).send('User not found');
+//     }
+//     const payload = {
+//       tokens: [user.fcmToken],
+//       notification: {
+//         title: 'Basic Notification',
+//         body: 'This is a basic notification sent from the server!',
+//       },
+//       data: {
+//         receiverId: id,
+//         code: code,
+//         navigationTarget: 'VideoCall',
+//       }
+//     };
+//     const response = await admin.messaging().sendMulticast(payload);
+
+//     console.log('Successfully sent multicast notification:', response);
+//     res.status(200).send('Multicast notification sent successfully');
+//   } catch (error) {
+//     console.error('Error sending multicast notification:', error);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
 
