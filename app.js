4fe15a3fa69957b7a2f4ee2a08edcfa64ca2407a
Aman2104 <amanyara88@gmail.com>
2024-02-07 20:25:24 +0530

    notifivcations

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
+const User= require('./models/User')
 
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
